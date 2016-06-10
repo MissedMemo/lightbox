b12fc99c64068bc795f8921efa4aba76e167d5b1
AndyL <missedmemo@gmail.com>
2016-06-10 11:44:03 -0700

    display query result item 'title' as image caption in overlay

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var searchButton = document.querySelector('.search-button');
-var searchField  = document.querySelector('.search-panel input');
-var imageList    = document.querySelector('.image-list');
-var overlay      = document.querySelector('#overlay');
-var overlayImage = document.querySelector('#overlay .image-area');
-var closeButton  = document.querySelector('#overlay .close-button');
+var searchButton   = document.querySelector('.search-button');
+var searchField    = document.querySelector('.search-panel input');
+var imageList      = document.querySelector('.image-list');
+var overlay        = document.querySelector('#overlay');
+var overlayImage   = document.querySelector('#overlay .image-area');
+var overlayCaption = document.querySelector('#overlay .caption-bar span');
+var closeButton    = document.querySelector('#overlay .close-button');
 
 searchButton.disabled = true; // init to disabled
 
@@ -28,6 +29,7 @@ imageList.addEventListener( 'click', function(e) {
   if( e.target.src !== undefined ) {
     overlay.style.display = 'block';
     overlayImage.style.backgroundImage = 'url( ' + e.target.src + ')';
+    overlayCaption.textContent = e.target.dataset.caption;
   }
   e.stopPropagation();
 }, false );
@@ -45,15 +47,17 @@ function getImages( searchTerms ) {
   var query = 'https://www.googleapis.com/customsearch/v1?'
             + 'key=AIzaSyDRNg12al500nvBg4w9vXxHxqMt4iVPgLA'
             + '&cx=013785967554816369765:m8ndxwd7vzw'
+            + '&safe=medium'
             + '&q=' + searchTerms;
   
   // Google API limits results to 10 per query, so issue multiple queries to fill page
-  for( var page = 1; page < 51; page += 10 ) {
+  for( var page = 41; page < 51; page += 10 ) {
 
     callAjax( query + '&start=' + page, function(results) {
       results.items.forEach( function(item) {
+        //console.log( 'item:', item );
         if( item.pagemap && item.pagemap.cse_thumbnail ) {
-          insertImage( item.pagemap.cse_thumbnail[0].src );
+          insertImage( item.pagemap.cse_thumbnail[0].src, item.title );
         }
       });
     });
@@ -62,10 +66,11 @@ function getImages( searchTerms ) {
 }
 
 
-function insertImage( url ) {
+function insertImage( url, caption ) {
 
   var img = document.createElement('img');
   img.setAttribute( 'src', url );
+  img.dataset.caption = caption;
 
   var div = document.createElement('div');
   div.className = 'img-container';
