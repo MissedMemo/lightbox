68405dd16901460d79683c77c29d50401962a025
AndyL <missedmemo@gmail.com>
2016-06-12 20:09:37 -0700

    refactored nav arrow styles

--- js/lightbox.js
+++ js/lightbox.js
@@ -11,8 +11,8 @@
   overlay.innerHTML = '<div id="lightbox">'
                     +   '<a href="#" class="close-button">X</a>'
                     +   '<div class="image-area">'
-                    +     '<button class="imagePrevious">&lsaquo;</button>'
-                    +     '<button class="imageNext">&rsaquo;</button>'
+                    +     '<button class="nav-arrow arrow-previous">&lsaquo;</button>'
+                    +     '<button class="nav-arrow arrow-next">&rsaquo;</button>'
                     +   '</div>'
                     +   '<div class="caption-bar">'
                     +     '<span>( no caption )</span>'
@@ -37,15 +37,15 @@
      appended to the DOM before acquiring references below. A production-quality
      implementation might want to use some element-level equivalent to 'onload',
      or even explicitly add each individual element, attribute, etc. -- but the
-     ability to view the entire overlay 'teamplate' is a huge convenience.
+     ability to view & edit the lightbox as a 'teamplate' is a huge convenience.
      see: http://stackoverflow.com/a/16726669
   */
   var closeButton  = document.querySelector('#lightbox .close-button');
   var lightbox     = document.querySelector('#lightbox');
   var imageArea    = document.querySelector('#lightbox .image-area');
+  var previous     = document.querySelector('#lightbox .arrow-previous');
+  var next         = document.querySelector('#lightbox .arrow-next');
   var imageCaption = document.querySelector('#lightbox .caption-bar span');
-  var previous     = document.querySelector('#lightbox .imagePrevious');
-  var next         = document.querySelector('#lightbox .imageNext');
 
 
   closeButton.addEventListener( 'click', function() {
